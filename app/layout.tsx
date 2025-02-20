8a6f94ce9596584c52a6f8afb5b16b66c1f3a38c
Sadlineee <sad1ineee@gmail.com>
2025-02-20 20:38:07 +0500

    counter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from 'next'
 import { State, Styled, Theme } from '@/lib'
 import { oswald } from '@/fonts/fonts'
 import { ProviderProps } from '@/types/ProviderProps'
+
+export const metadata: Metadata = {
+  title: 'counter'
+}
 
 export default function RootLayout({ children }: Readonly<ProviderProps>) {
   return (
